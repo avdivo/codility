e6ae926e2e0c633f643bd24f3ce56935fb8d76b9
avdivo <avdivo@mail.ru>
2023-01-25 17:56:23 +0300

    MissingInteger.py ok

--- MissingInteger.py
+++ MissingInteger.py
@@ -1,11 +1,31 @@
-def solution(A):
-    A.append(0)
-    A.sort(reverse=True)
-    A = A[:A.index(0)]
-    if not A:
-        return 1
-    return min(set(range(1, max(A)+2)) ^ set(A))
+import random, time
 
-print(solution([1, 3, 6, 4, 1, 2]))
+
+def solution(A):
+    # ts = time.time()
+    # clear = set(filter(lambda x: x > 0, A))
+    # print(time.time() - ts)
+    #
+    # ts = time.time()
+    # print(time.time() - ts)
+    # if not clear:
+    #     return 1
+    # out = min(set(range(1, max(clear)+2)) ^ clear)
+    # print(time.time() - ts)
+    #
+    # return out
+
+    clear = sorted(list(set(filter(lambda x: x > 0, A))))
+    for i in range(len(clear)+1):
+        try:
+            if i+1 != clear[i]:
+                return i + 1
+        except:
+            return i + 1
+
+
+
+s = [random.randint(0, 10000) for _ in range(1000005)]
+print(solution(s))
 print(solution([1, 2, 3]))
 print(solution([-1, -3]))
