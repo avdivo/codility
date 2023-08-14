e9295615b476780a6ad2187a1fbe714167c82a1f
avdovo <avdivo@mail.ru>
2023-08-14 17:25:05 +0300

    Решена Pi Code Challenge

--- PiCodeChallenge.py
+++ PiCodeChallenge.py
@@ -1,31 +1,67 @@
-P = 'amz'
-Q = 'amz'
+import itertools
+import random
 
-position = list(zip(P, Q))
+# letters = 'abcdefghijklmnopqrst'
+# n = 100000
+# P = [random.choice(letters) for i in range(n)]
+# Q = [random.choice(letters) for i in range(n)]
 
-# Сколько раз конкретная буква может встречаться в строке
-letters = dict()
-for one, two in position:
-    letters.setdefault(one, 0)
-    letters[one] += 1
+P = 'dcba'
+Q = 'cbad'
+
+P = 'axxz'
+Q = 'yzwy'
+
+P = 'xyz'
+Q = 'yxe'
+
+P = 'abcdef'
+Q = 'bcdefa'
+
+P = 'aaaaabcdd'
+Q = 'efghighii'
+
+P = 'bgeafdd'
+Q = 'geafddc'
+
+P = 'aaaacbcddd'
+Q = 'bbbbacdaaa'
+
+P = 'dddabc'
+Q = 'abcefx'
+
+# Переписываем пары букв в set отсортированных кортежей (каждая комбинация получится по 1 разу)
+screening = set()
+for a in zip(P, Q):
+    screening.add(tuple(sorted(a)))
+
+# Готовим словари весов и позиций букв
+weight = dict()
+position = dict()
+for i, (one, two) in enumerate(screening):
+    weight.setdefault(one, 0)
+    weight[one] += 1
+    position.setdefault(one, set())
+    position[one].add(i)
     if one != two:
-        letters.setdefault(two, 0)
-        letters[two] += 1
+        weight.setdefault(two, 0)
+        weight[two] += 1
+        position.setdefault(two, set())
+        position[two].add(i)
+letters = [key for key in weight]
 
-# Строим лучшую строку, но поскольку нужно только количество, то можно использовать множество
-# В лучшей строке выбираются буквы, которые встречаются чаще всего,
-# при одинаковом количестве выбираются буквы, которые уже добавлялись или любая из них, если не добавлялись
-string = set()
-for one, two in position:
-    if letters[one] > letters[two]:
-        string.add(one)
-    elif letters[one] < letters[two]:
-        string.add(two)
-    else:
-        if one in string:
-            continue
-        if two in string:
-            continue
-        string.add(one)
+total_len = len(screening)  # Длина строки, которую нужно получить
 
-print(len(string))
+# Перебираем все комбинации букв, начиная с самых частых. Первая комбинация, которая покроет все позиции
+# будет искомой строкой, возвращаем ее длину
+for i in range(1, len(screening)+1):
+    combinations = itertools.combinations(letters, i)
+    for combo in combinations:
+        sample = set()
+        for l in combo:
+            sample = sample.union(position[l])
+        if len(sample) == total_len:
+            print(i)
+            exit()
+
+
